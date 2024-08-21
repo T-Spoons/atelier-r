c186f2ed55b17f1b3dd0c54ef09f9264351edc60
TSpoons <thierry.spooner@polymtl.ca>
2024-08-20 21:15:15 -0400

    Add part 1 solutions

--- partie1/solutions.r
+++ partie1/solutions.r
@@ -13,43 +13,40 @@ data <- ToothGrowth
 # Séparer les données en deux dataframes selon la colonne "supp". Une dataframe
 # pour les vitamines C "VC" et l'autre pour le jus d'orange "OJ".
 
-#vitamineC <- ...
-#jusOrange <- ...
+vitamineC <- subset(data, supp == "VC")
+jusOrange <- subset(data, supp == "OJ")
 
 
 # PARTIE 2.
-# Créer 2 nouvelles dataframes contenant seulement les colonnes "len" et "supp"
-# de vitamineC et jusOrange
+# Retirer la colonne "supp" des deux dataframes, car elle n'est plus nécessaire.
 
-# ...
+len <- vitamineC$len
+dose <- vitamineC$dose
+vitamineC <- data.frame(len, dose)
 
-#vitamineC <-  data.frame(...)
-#jusOrange <-  data.frame(...)
-  
+len <- jusOrange$len
+dose <- jusOrange$dose
+jusOrange <- data.frame(len, dose)
+
 # PARTIE 3.
 # Utiliser la fonction plot pour faire des graphiques pour nos deux dataframes
 # X étant la dose et Y étant la longueur
 
-# plot(...)
-# plot(...)
-
+plot(vitamineC$dose, vitamineC$len)
+plot(jusOrange$dose, jusOrange$len)
 
 # PARTIE 4.
 # Vous aimeriez avoir un seul point par valeur de dose, pour ce faire, vous
 # pensez faire la moyenne de toutes les longeurs appartenant à une dose.
 # Utilisez la fonction "aggregate" pour regrouper chaque valeur de dose et pour
-# y appliquer la fonction "mean". La fonction help pourrait vous aider!
+# y appliquer la fonction "mean".
 
-# moyVitamineC <- aggregate(...)
-# moyJusOrange <- aggregate(...)
+moyVitamineC <- aggregate(vitamineC, list(vitamineC$dose), mean)
+moyJusOrange <- aggregate(jusOrange, list(jusOrange$dose), mean)
 
 # PARTIE 5.
 # Utilisez la fonction plot à nouveau et comparez les résultats pour trouver le
 # meilleur supplément!
 
-# plot(...)
-# plot(...)
-
-
-
-
+plot(moyVitamineC$dose, moyVitamineC$len)
+plot(moyJusOrange$dose, moyJusOrange$len)
